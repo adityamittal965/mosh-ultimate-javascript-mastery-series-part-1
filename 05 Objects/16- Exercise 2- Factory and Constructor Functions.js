1715b9e8fc4463ea6c2795ce5e594d47460b559b
Aditya Mittal <62979926+adityamittal965@users.noreply.github.com>
2023-09-19 17:33:49 +0530

    Update 16- Exercise 2- Factory and Constructor Functions.js

--- 05 Objects/16- Exercise 2- Factory and Constructor Functions.js
+++ 05 Objects/16- Exercise 2- Factory and Constructor Functions.js
@@ -1,7 +1,7 @@
 // 16- Exercise 2- Factory and Constructor Functions
 
 // Factory function
-function cretaAddress(street, city, zipCode) {
+function createAddress(street, city, zipCode) {
     return  {
         street,
         city,
@@ -17,7 +17,7 @@ function Address(street, city, zipCode) {
 };
 
 
-let address1 = cretaAddress('Rua Afonso Braz 505', 'São Paulo', '04511-011');
+let address1 = createAddress('Rua Afonso Braz 505', 'São Paulo', '04511-011');
 console.log(address1);
 
 let address2 = new Address('Rua Augusta 2099', 'São Paulo', '01413-000');
